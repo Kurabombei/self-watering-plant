6e5c28a0ae1b45b863de5c48e3db6823f54e8cba
Дима Прокопчук <47715077+Rabb1t410@users.noreply.github.com>
2019-05-27 18:30:39 +0300

    Create hueta.java
    loh

---
+++ hueta.java
@@ -0,0 +1,9 @@
+package hueta;
+
+public class Hueta{
+  public static void main(String[] args){
+      String hueta = new String;
+      hueta = "hueta";
+      System.out.print(hueta * 10);
+    }
+ }
